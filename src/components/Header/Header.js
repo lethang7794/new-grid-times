934b9a05c92ffb210c6c4123cdbe51a18bed88e9
Thang Le Quang <lethang7794@gmail.com>
2022-02-06 22:09:39 +0700

    Exercise 1: Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,7 +29,19 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <DesktopActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </DesktopActionGroup>
         <Logo />
+        <DesktopUserActions>
+          <Button>Subscribe</Button>
+          <Link href=''>Already a subscriber?</Link>
+        </DesktopUserActions>
       </MainHeader>
     </header>
   );
@@ -39,6 +51,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -65,6 +81,44 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    margin-top: 48px;
+    margin-bottom: 72px;
+  }
+
+  @media ${QUERIES.desktopAndUp} {
+    height: 98px;
+    justify-content: space-between;
+    margin-top: 16px;
+    margin-bottom: 80px;
+  }
+`;
+
+const DesktopActionGroup = styled(ActionGroup)`
+  display: none;
+  @media ${QUERIES.desktopAndUp} {
+    display: flex;
+  }
+`;
+
+const DesktopUserActions = styled.div`
+  align-self: flex-end;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+
+  display: none;
+  @media ${QUERIES.desktopAndUp} {
+    display: flex;
+  }
+`;
+
+const Link = styled.a`
+  color: var(--color-gray-900);
+  font-style: italic;
+  font-weight: var(--weight-normal);
+  font-size: ${14 / 16}rem;
 `;
 
 export default Header;
